977ebc153369d8ce8b98627bdaebea020a765209
Necdet UYGUR <necdet.uygur@gmail.com>
2023-08-26 23:08:04 +0300

    Server

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const fs = require("fs");
+const app = express();
+const path = require("path");
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "/index.html"));
+});
+
+// GET istekleri için olay akışını ayarla
+app.get("/events", (req, res) => {
+  res.setHeader("Content-Type", "text/event-stream");
+  res.setHeader("Cache-Control", "no-cache");
+  res.setHeader("Connection", "keep-alive");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  let counter = 0;
+  let interValID = setInterval(() => {
+    counter++;
+    if (counter >= 10) {
+      clearInterval(interValID);
+      res.end(); // terminates SSE session
+      return;
+    }
+    res.write(`data: ${JSON.stringify({ num: counter })}\n\n`); // res.write() instead of res.send()
+  }, 1000);
+
+  // If client closes connection, stop sending events
+  res.on("close", () => {
+    console.log("client dropped me");
+    clearInterval(interValID);
+    res.end();
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Sunucu ${PORT} portunda çalışıyor.`);
+});
